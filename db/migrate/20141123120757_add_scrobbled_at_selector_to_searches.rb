5e6c8702b2d2a9ecea5aed2169211a1da72656e7
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-03 21:49:41 +0800

    Delete db directory

--- db/migrate/20141123120757_add_scrobbled_at_selector_to_searches.rb
+++
@@ -1,5 +0,0 @@
-class AddScrobbledAtSelectorToSearches < ActiveRecord::Migration
-  def change
-    add_column :searches, :scrobbled_at_selector, :string
-  end
-end
--- db/migrate/20141123120825_add_scrobbled_at_to_scrobbles.rb
+++
@@ -1,5 +0,0 @@
-class AddScrobbledAtToScrobbles < ActiveRecord::Migration
-  def change
-    add_column :scrobbles, :scrobbled_at, :datetime
-  end
-end
